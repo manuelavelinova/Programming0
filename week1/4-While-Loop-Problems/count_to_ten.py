c2d39a22f0fb44db71f8c503e578c452285ed0d9
Manuela Velinova <manuelavelinova@gmail.com>
2015-03-09 21:40:42 +0200

    Create count_to_ten.py

---
+++ week1/4-While-Loop-Problems/count_to_ten.py
@@ -0,0 +1,17 @@
+start = 1
+end = 10
+
+print("Counting from 1 to 10")
+
+while start <= end:
+    print(start)
+    start += 1
+
+print("Counting from 10 to 1")
+
+start = 10
+end = 0
+
+while start >= end:
+    print(start)
+    start -= 1
