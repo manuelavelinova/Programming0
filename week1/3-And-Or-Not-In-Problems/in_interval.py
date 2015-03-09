984ba1686401518fd388a696dc94fe126fdb2724
Manuela Velinova <manuelavelinova@gmail.com>
2015-03-09 21:35:53 +0200

    Create in_interval.py

---
+++ week1/3-And-Or-Not-In-Problems/in_interval.py
@@ -0,0 +1,18 @@
+a = input("Enter a: ")
+b = input("Enter b: ")
+x = input("Enter x: ")
+
+a = int(a)
+b = int(b)
+x = int(x)
+
+if x == a:
+    print("The number is equal to the lower bound of the interval")
+elif x == b:
+    print("The number is equal to the upper bound of the interval")
+elif x > a and x < b:
+    print("The number is in the interval")
+elif x < a:
+    print("The number is outside the interva,x < a")
+elif x > b:
+    print("The number is outside the interval, x > b")
