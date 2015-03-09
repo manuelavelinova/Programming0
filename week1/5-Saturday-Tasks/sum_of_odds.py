f0d675f278fcb0a114a60ee54b95cf654f09619a
Manuela Velinova <manuelavelinova@gmail.com>
2015-03-09 22:14:10 +0200

    Create sum_of_odds.py

---
+++ week1/5-Saturday-Tasks/sum_of_odds.py
@@ -0,0 +1,13 @@
+n = input("Enter n: ")
+n = int(n)
+
+start = 1
+sum = 0
+
+while start <= n:
+    if start % 2 == 1:
+        sum += start
+        start += 1
+    else:
+        start += 1
+print(sum)
