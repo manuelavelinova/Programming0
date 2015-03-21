383307fe996c62f0b07bebe35e735877122a3471
Manuela Velinova <manuelavelinova@gmail.com>
2015-03-21 18:16:23 +0200

    Create hash_them.py

---
+++ week4/2-Hash-Them/hash_them.py
@@ -0,0 +1,14 @@
+def hash_them(keys,values):
+    result = {}
+    index = 0
+    
+    for key in keys:
+        if index < len(values):
+            result[key] = values[index]
+        else:
+            result[key] = None
+        index += 1
+
+    return result
+
+print(hash_them(["Ivan","Maria"],[1]))
