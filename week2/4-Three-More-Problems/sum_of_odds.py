ede84db93eb487831394f2d80bea9213ab23d216
Manuela Velinova <manuelavelinova@gmail.com>
2015-03-21 11:44:15 +0200

    Create sum_of_odds.py

---
+++ week2/4-Three-More-Problems/sum_of_odds.py
@@ -0,0 +1,14 @@
+n = input("Enter n: ")
+n = int(n)
+
+start = 1
+sum = 0
+
+while start <= n:
+    if start % 2 != 0:
+        sum += start
+        start += 1
+    else:
+        start +=1
+print(sum)
+        
