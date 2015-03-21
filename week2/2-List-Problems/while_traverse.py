e691ea68a7cc9f0a4e199df995a8fbee8475f08e
Manuela Velinova <manuelavelinova@gmail.com>
2015-03-21 11:15:40 +0200

    Create while_traverse.py

---
+++ week2/2-List-Problems/while_traverse.py
@@ -0,0 +1,14 @@
+books =["learn you a Haskell",
+        "The healthy programmer",
+        "Code Complete",
+        "The pragmatic programmer",
+        "Pro git",
+        "Introduction to Algorithms",
+        "Concrete Mathematics"]
+
+index = 0
+
+while index < len(books):
+    print(books[index])
+    index += 1
+        
