1fe31133cf94fab9b2414d09e47c377ef1ecaf98
Manuela Velinova <manuelavelinova@gmail.com>
2015-03-21 11:43:54 +0200

    Create sum_of_evens.py

---
+++ week2/4-Three-More-Problems/sum_of_evens.py
@@ -0,0 +1,13 @@
+n = input("Enter n : ")
+n = int(n)
+
+start = 2
+sum = 0
+
+while start <= n:
+    if start % 2 == 0:
+        sum += start
+        start +=1
+    else:
+        start += 1
+print(sum)
